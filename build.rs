ee7500d0ea95ff59151f4ccc0b849ecc477062d5
Keval <bhogayatakb@gmail.com>
2022-07-27 18:37:10 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/metrics.proto")?;
-    tonic_build::compile_protos("proto/helloworld.proto")?;
-
     Ok(())
 }
